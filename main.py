29f33df6f8f42dc29421eab0554cb60bcb9ab5d9
Maria <125877908+arrryh@users.noreply.github.com>
2024-02-15 15:51:19 +0700

    add main file

---
+++ main.py
@@ -0,0 +1,46 @@
+# print('Hello', 'World!')
+
+print('Hello', '\tWorld! \n')  # print('Hello', 'World!')
+print('Hello', 'World!')  # print('Hello', 'World!')
+print('Hello', 'World!')  # print('Hello', 'World!')
+
+# print('Hello', 'World!')
+print(
+    'Hello', 
+    'World!'
+    )  
+
+if 3 > 2:
+    print('Yes, 3 > 2')
+    if 3 < 4:
+        print('but 3 < 4')
+
+
+print('Hello, John!')
+
+# print numbers of 0 to 10
+# for i in range(10):
+    # print(i)
+
+a = 1
+b = 2
+c = 5.6
+
+s = 'Hello John!'
+
+print(a, b, c, s)
+
+a = 'aaaa'
+
+print('a:', type(a))
+print('b:', type(b))
+print('c:', type(c))
+print('s:', type(s))
+
+b = float(b)
+c = str(c)
+d = int(2.9999999999999999)
+
+print('b:', type(b), b)
+print('c:', type(c), c)
+print('d:', type(d), d)
